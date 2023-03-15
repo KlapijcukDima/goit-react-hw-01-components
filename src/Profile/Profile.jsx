d89021834c2d18d6701571e19ca7f0b064d0d760
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-03-15 22:22:18 +0200

    Update Profile.jsx

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -32,3 +32,11 @@ export const Profile = ({ username, tag, location, avatar, stats}) => {
 </div>
     )
 }
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
